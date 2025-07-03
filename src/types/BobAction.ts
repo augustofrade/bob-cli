0cde44aa0824fbf6b337f61afc1235f5881f12ae
augustofrade <aquaaticnaotrava@gmail.com>
2025-07-02 22:49:56 -0300

    Add new BobAction types

--- src/types/BobAction.ts
+++ src/types/BobAction.ts
@@ -1,1 +1,11 @@
 export type BobAction = "text" | "file" | "dir" | "script" | "qr";
+
+export interface BobActionData {
+  action_name: string;
+  content: string;
+  type: BobAction;
+  description?: string;
+  learntAt: Date;
+}
+
+export type CreateBobAction = Omit<BobActionData, "learntAt">;
