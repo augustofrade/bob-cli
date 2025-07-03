4711cff3133787a655d8065bab3d2dba9d56faff
augustofrade <aquaaticnaotrava@gmail.com>
2025-07-02 22:49:39 -0300

    Add action saving and fetching to DataManager

--- src/core/DataManager/index.ts
+++ src/core/DataManager/index.ts
@@ -1,7 +1,39 @@
+import { BobActionData, CreateBobAction } from "../../types/BobAction";
+import JsonFS from "../JsonFS";
+
 export default class DataManager {
   public static instance: DataManager;
+  private static readonly learntActionsFile = "learntActions.json";
+  private learntActions: Array<BobActionData> = [];
 
   private constructor() {}
+
+  public getlearntActions(): Promise<Array<BobActionData>> {
+    return new Promise((resolve, reject) => {
+      if (this.learntActions.length > 0) {
+        return resolve(this.learntActions);
+      }
+
+      JsonFS.read<BobActionData[]>(DataManager.learntActionsFile)
+        .then((data) => {
+          this.learntActions = data || [];
+          resolve(this.learntActions);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  public saveLearntAction(action: CreateBobAction): Promise<void> {
+    const data: BobActionData = {
+      ...action,
+      learntAt: new Date(),
+    };
+    this.learntActions.push(data);
+
+    return JsonFS.write(DataManager.learntActionsFile, data);
+  }
 
   /**
    * Singleton instance of DataManager
