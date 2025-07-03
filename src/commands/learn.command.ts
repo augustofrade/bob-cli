118982bc38bc21697258a9f33f99301e42d769ac
augustofrade <aquaaticnaotrava@gmail.com>
2025-07-02 23:11:16 -0300

    Add action knowledge verification

--- src/commands/learn.command.ts
+++ src/commands/learn.command.ts
@@ -4,13 +4,26 @@ import { BobActionType } from "../types/BobAction";
 
 export default async function learnCommand(args: ArgumentsCamelCase<LearnCommandArgs>) {
   const actionManager = ActionManager.Instance;
+  const learntActions = await actionManager.getlearntActions();
+  if (learntActions[args.action_name]) {
+    console.error(`I've already learnt the action "${args.action_name}"!\n`);
+    return;
+  }
 
-  actionManager.saveLearntAction({
-    actionName: args.action_name,
-    content: args.content,
-    type: args.type,
-    description: args.description,
-  });
+  try {
+    await actionManager.saveLearntAction({
+      actionName: args.action_name,
+      content: args.content,
+      type: args.type,
+      description: args.description,
+    });
+    console.log(`I've learnt the action "${args.action_name}" of the type ${args.type}!\n`);
+  } catch (error) {
+    console.error(
+      `Something happened while I tried learning the action "${args.action_name}".\n\n`
+    );
+    console.log(error);
+  }
 }
 
 interface LearnCommandArgs {
