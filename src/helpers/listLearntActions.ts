4d81df2fd9403553c48ea908fa68ef44a6b8114d
augustofrade <aquaaticnaotrava@gmail.com>
2025-07-04 11:20:29 -0300

    Add default message to listLearntActions helper function that is displayed if no actions were learnt yet

--- src/helpers/listLearntActions.ts
+++ src/helpers/listLearntActions.ts
@@ -1,11 +1,19 @@
 import ActionManager from "../core/ActionManager";
 
 export default async function listLearntActions() {
-  console.log("\nHere's what I know how to do:");
   const actions = await ActionManager.Instance.getLearntActionsArray();
+  if (actions.length === 0) {
+    console.log("I haven't learnt any actions yet. You can teach me something new with");
+    console.log("bob learn <action_name> <content> --type <type>\n");
+    console.log("For more information, run 'bob help learn'.");
+    return;
+  }
+
+  console.log("\nHere's what I know how to do:");
   actions.forEach((action) => {
     if (action.description)
       console.log(`- ${action.actionName}\n    Description:  ${action.description}`);
     else console.log(`- ${action.actionName}`);
   });
+  console.log("");
 }
