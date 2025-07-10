803cf46b8d82ace85d9170f93e08fc6c1f39f9ec
augustofrade <aquaaticnaotrava@gmail.com>
2025-07-10 00:36:27 -0300

    Add depth and file displaying verification to tree command recursive directory listing function

--- src/commands/tree.command.ts
+++ src/commands/tree.command.ts
@@ -35,11 +35,13 @@ function readDir(directory: string, info: RecursiveDirInfo, level = 0) {
     const displayName = `${identation}${spaceBetween}${element.name}`;
     if (!info.showHidden && element.name.startsWith(".")) return;
 
-    if (element.isDirectory()) {
-      console.log(chalk.bold(displayName));
+    if (!element.isDirectory() && info.showFiles) {
+      return console.log(displayName);
+    }
+    console.log(chalk.bold(displayName));
+
+    if (level < info.depth) {
       readDir(path.join(directory, element.name), info, level + 1);
-    } else {
-      console.log(displayName);
     }
   });
 }
